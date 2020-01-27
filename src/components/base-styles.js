c016090ee23fcf8a5ebf367bae9c994e3e95f45a
Josep Martins <josepmartins@Joseps-MacBook-Pro.local>
2020-01-27 22:35:01 +0100

    Host Libre Baskerville font

--- src/components/base-styles.js
+++ src/components/base-styles.js
@@ -1,13 +1,32 @@
 import { createGlobalStyle } from 'styled-components'
+import LibreBaskervilleRegularWoff from '../fonts/libre-baskerville-regular.woff'
+import LibreBaskervilleRegularWoff2 from '../fonts/libre-baskerville-regular.woff2'
+import LibreBaskervilleItalicWoff from '../fonts/libre-baskerville-italic.woff'
+import LibreBaskervilleItalicWoff2 from '../fonts/libre-baskerville-italic.woff2'
 
 const BaseStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'Libre Baskerville';
+    font-style: normal;
+    font-weight: 400;
+    src: local('Libre Baskerville'), local('LibreBaskerville-Regular'),
+        url('${LibreBaskervilleRegularWoff2}') format('woff2'), url('${LibreBaskervilleRegularWoff}') format('woff');
+  }
+
+  @font-face {
+    font-family: 'Libre Baskerville';
+    font-style: italic;
+    font-weight: 400;
+    src: local('Libre Baskerville Italic'), local('LibreBaskerville-Italic'),
+        url('${LibreBaskervilleItalicWoff2}') format('woff2'), url('${LibreBaskervilleItalicWoff}') format('woff');
+  }
+
   html {
     font-size: 100%;
     line-height: 1.25;
   }
 
   body {
-    @import url('https://fonts.googleapis.com/css?family=Libre+Baskerville&display=swap');
     font-family: 'Libre Baskerville', serif;
     font-weight: 400;
     margin: 0;
