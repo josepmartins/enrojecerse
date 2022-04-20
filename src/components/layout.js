dbea576de5ef34c0cc275547218b3d97d00be0c1
josepmartins <jsp.mrtns@gmail.com>
2022-04-20 12:19:57 +0200

    feat(SEO): Add splitbee

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from "react"
 import BaseStyles from "../components/base-styles"
 import Header from "../components/header"
 import styled from "styled-components"
+import Helmet from "react-helmet"
 
 const Wrapper = styled.div`
   margin: 64px var(--layout-side-margins) 2vh;
@@ -9,11 +10,14 @@ const Wrapper = styled.div`
 
 class Layout extends React.Component {
   render() {
-    const { children } = this.props;
-    const date = this.props.date ? this.props.date : null;
+    const { children } = this.props
+    const date = this.props.date ? this.props.date : null
 
     return (
       <Wrapper>
+        <Helmet>
+          <script async src="https://cdn.splitbee.io/sb.js"></script>
+        </Helmet>
         <BaseStyles />
         <Header date={date} />
         <main>{children}</main>
